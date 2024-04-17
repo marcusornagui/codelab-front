b068816fd1fd8913964458c396ded3350483c809
Marcus Ornagui <marcus.ornagui@vrsoft.com.br>
2024-04-17 14:39:20 -0300

    - Implementado tela de Login.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import {  Component} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HttpClientModule } from '@angular/common/http';
 
-@Component({
+
+@Component ({
   selector: 'cl-root',
   standalone: true,
-  imports: [],
+  imports: [ HttpClientModule,
+    MatButtonModule, MatCardModule,
+    MatInputModule, MatIconModule, MatSlideToggleModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  nome = 'Henrique';
 
-  printaNome() {
-    console.log(this.nome);
-  }
+export class AppComponent  {
+  ocultarSenha = true;
 }
